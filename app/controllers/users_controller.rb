97ccbed0581feb7f0cd884566512e938c12ae933
James <james.ryan.mcguire@gmail.com>
2021-12-23 23:32:44 -0500

    Changed block function conditional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
-                                        :following, :followers]
+                                        :following, :followers, :block]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -45,6 +45,12 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  def block 
+    User.find(params[:id]).block
+    flash[:success] = "User is blocked"
     redirect_to users_url
   end
 
