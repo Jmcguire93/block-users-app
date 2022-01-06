77249a87736f476a022445ccdcd8309b9f1f2f8a
SingleOps <5115257+SingleOps@users.noreply.github.com>
2022-01-05 18:07:24 -0600

    Add hacker

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,3 +34,19 @@ following = users[2..50]
 followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+Relationship.all.find_each do |r|
+  new_time = Time.zone.now - rand(1.year)
+  r.update_columns(updated_at: new_time, created_at: new_time)
+end
+
+# Simulate Samy Worm
+samy = User.all.order("RANDOM()").first
+samy.update(name: "Samy Kamkar",
+             email: "everyones-hero@singleops.com",
+             password: "SamyWorm2005",
+             password_confirmation: "SamyWorm2005")
+User.where.not(email: "everyones-hero@singleops.com").find_each do |user|
+  user.follow(samy) unless user.following? samy
+end
+
